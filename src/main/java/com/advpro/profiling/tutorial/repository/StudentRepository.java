f244500f62ab3ec0c2bf59c01d1d082b97e78f24
Acid <Acid-aja@users.noreply.github.com>
2026-04-25 19:50:53 +0700

    [Refactoring] - Optimizing method getAllStudentWithCourses and findStudentWithHighestGpa and joinStudentNames

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -2,11 +2,17 @@ package com.advpro.profiling.tutorial.repository;
 
 import com.advpro.profiling.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-/**
- * @author muhammad.khadafi
- */
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    
+    Optional<Student> findFirstByOrderByGpaDesc();
+
+    @Query("SELECT s.name FROM Student s")
+    List<String> findAllStudentNames();
 }
